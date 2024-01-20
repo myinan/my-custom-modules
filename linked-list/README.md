497cfb098222d9e7c47719b9091700d757197c53
myinan <m.yasir.inan@gmail.com>
2024-01-20 13:47:08 +0300

    Add linked list

---
+++ linked-list/README.md
@@ -0,0 +1,28 @@
+Custom linked list module.
+
+`append(value)` adds a new node containing value to the end of the list.
+
+`prepend(value)` adds a new node containing value to the start of the list.
+
+`size` returns the total number of nodes in the list.
+
+`headNode` returns the first node in the list.
+
+`tailNode` returns the last node in the list.
+
+`getNodeAt(index)` returns the node at the given index.
+
+`pop` removes the last element from the list.
+
+`contains(value)` returns true if the passed in value is in the list and otherwise returns false.
+
+`indexOf(value)` returns the index of the node containing value, or null if not found.
+
+`toString` represents your LinkedList objects as strings, so you can print them out and preview them in the console.
+The format should be: ( value ) -> ( value ) -> ( value ) -> null
+
+`insertAt(value, index)` inserts a new node with the provided value at the given index.
+
+`removeAt(index)` removes the node at the given index.
+
+`clear()` clears the linked list.
---
+++ linked-list/linkedList.js
@@ -0,0 +1,158 @@
+import Node from "./node";
+
+export default class LinkedList {
+  constructor(head = null) {
+    this.head = head;
+  }
+
+  append(value) {
+    const newNode = new Node(value);
+    this.tailNode.next = newNode;
+  }
+
+  prepend(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+  }
+
+  get size() {
+    if (!this.head) return new Error("List is empty.");
+    let count = 0;
+    let node = this.head;
+    while (node) {
+      count += 1;
+      node = node.next;
+    }
+    return count;
+  }
+
+  get headNode() {
+    if (!this.head) return new Error("List is empty.");
+    return this.head;
+  }
+
+  get tailNode() {
+    if (!this.head) return new Error("List is empty.");
+    let lastNode = this.head;
+    if (lastNode) {
+      while (lastNode.next) {
+        lastNode = lastNode.next;
+      }
+    }
+    return lastNode;
+  }
+
+  getNodeAt(index) {
+    if (this.head && index === 0) return this.head;
+    if (!this.head) return new Error("List is empty.");
+    let count = 0;
+    let currentNode = this.head.next;
+    while (currentNode) {
+      count += 1;
+      if (index === count) return currentNode;
+      currentNode = currentNode.next;
+    }
+    return new Error(`Node at index ${index} does not exist.`);
+  }
+
+  pop() {
+    if (!this.head) return new Error("The list is empty.");
+    let cur = this.head;
+    let tmp = cur;
+    while (cur.next !== null) {
+      tmp = cur;
+      cur = cur.next;
+    }
+    tmp.next = null;
+    if (tmp === cur) {
+      tmp = null;
+      this.head = null;
+    }
+    return tmp;
+  }
+
+  contains(value) {
+    let cur = this.head;
+    while (cur) {
+      if (cur.value === value) return true;
+      cur = cur.next;
+    }
+    return new Error("There are no nodes in the list with the searched value.");
+  }
+
+  indexOf(value) {
+    if (!this.head) return new Error("The list is empty.");
+    let cur = this.head;
+    let curIndex = 0;
+    while (cur) {
+      if (cur.value === value)
+        return `Searched value: ${value}, Index: ${curIndex}`;
+      cur = cur.next;
+      curIndex += 1;
+    }
+    return new Error("There are no nodes in the list with the searched value.");
+  }
+
+  toString() {
+    if (!this.head) return new Error("The list is empty.");
+    let cur = this.head;
+    let string = "";
+    while (cur) {
+      const stringToConcat = `( ${cur.value} ) => `;
+      string = string.concat(stringToConcat);
+      cur = cur.next;
+    }
+    return string.concat("null");
+  }
+
+  insertAt(value, index) {
+    const newNode = new Node(value);
+    let cur = this.head;
+    let tmp = cur;
+    let track = 0;
+    let indexInRange = false;
+    while (cur) {
+      if (track === index) {
+        tmp.next = newNode;
+        newNode.next = cur;
+        indexInRange = true;
+      }
+      tmp = cur;
+      cur = cur.next;
+      track += 1;
+    }
+    if (!indexInRange)
+      return new Error("Index is outside of the linked list boundries.");
+    return newNode;
+  }
+
+  removeAt(index) {
+    let cur = this.head;
+    let tmp = cur;
+    let track = 0;
+    let indexInRange = false;
+    let removedElement;
+    while (cur) {
+      if (track === index) {
+        tmp.next = cur.next;
+        cur.next = null;
+        removedElement = cur;
+        indexInRange = true;
+      }
+      tmp = cur;
+      cur = cur.next;
+      track += 1;
+    }
+    if (!indexInRange)
+      return new Error("Index is outside of the linked list boundries.");
+
+    return removedElement;
+  }
+
+  clear() {
+    if (!this.head) return new Error("The list is empty.");
+    this.head = null;
+    return this.head;
+  }
+}
